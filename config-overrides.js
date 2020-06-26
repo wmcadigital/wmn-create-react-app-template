6446b6ab7e88f53a8620f8871b0a726f5ff15938
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-06-26 15:36:46 +0100

    Updated sass to point to a root folder

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,6 @@
-// config-overrides.js
+// This file helps to reroute the webpack build through here first
+// This enables to modify the webbpack config without ejecting CRA
+// For more: https://www.npmjs.com/package/react-app-rewired
 
 const SassLintPlugin = require('sass-lint-webpack');
 
