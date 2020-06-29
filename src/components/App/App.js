f28b556af0bdd49c90fb53ec3de3476bae0bf3cf
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-06-29 10:41:51 +0100

    Updated intro page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,9 +7,18 @@ import s from './App.module.scss';
 function App() {
   return (
     <div className="wmnds-container wmnds-p-t-lg wmnds-p-b-lg wmnds-grid">
-      <Icon className={s.icon} iconName="modes-isolated-bus" />
-      <Icon className={s.icon} iconName="modes-isolated-metro" />
-      <Icon className={s.icon} iconName="modes-isolated-rail" />
+      <h2 className="wmnds-col-1">Hello West Midlands!</h2>
+      <div className="wmnds-col-1">
+        {/* Bus */}
+        <Icon className={`${s.icon} wmnds-col-1 wmnds-col-sm-1-3`} iconName="modes-isolated-bus" />
+        {/* Metro */}
+        <Icon
+          className={`${s.icon} wmnds-col-1 wmnds-col-sm-1-3`}
+          iconName="modes-isolated-metro"
+        />
+        {/* Train */}
+        <Icon className={`${s.icon} wmnds-col-1 wmnds-col-sm-1-3`} iconName="modes-isolated-rail" />
+      </div>
     </div>
   );
 }
