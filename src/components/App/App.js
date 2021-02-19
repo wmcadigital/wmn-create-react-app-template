e50c702bbba83dc4ff4ece35947eaa035f2725a2
Dayle Salmon <dayle.salmon@wmca.org.uk>
2021-02-19 13:51:58 +0000

    Updated react app to use vars for quicker setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,8 +7,26 @@ import s from './App.module.scss';
 function App() {
   return (
     <div className="wmnds-container wmnds-p-t-lg wmnds-p-b-lg wmnds-grid">
-      <h2 className="wmnds-col-1">Hello West Midlands!</h2>
       <div className="wmnds-col-1">
+        <h2>Hello West Midlands!</h2>
+        <p>Don&apos;t forget to:</p>
+        <ul>
+          <li>
+            Update the{' '}
+            <a
+              href="https://designsystem.wmnetwork.co.uk/docs/get-started/production/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              WMN Design System CSS to the latest version
+            </a>{' '}
+            on <strong>line:51 of index.html</strong>
+          </li>
+          <li>Change the page title</li>
+          <li>Change meta title and description</li>
+          <li>Update the breadcrumbs</li>
+        </ul>
+
         {/* Bus */}
         <Icon className={`${s.icon} wmnds-col-1 wmnds-col-sm-1-3`} iconName="modes-isolated-bus" />
         {/* Metro */}
